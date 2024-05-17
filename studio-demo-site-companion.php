f9ecc89a683237959c004a6066b63462b74d8367
Derek Blank <derekpblank@gmail.com>
2024-05-17 15:11:34 +1000

    Fix paragraph tag margin specificity on #studio-companion-notice

--- studio-demo-site-companion.php
+++ studio-demo-site-companion.php
@@ -59,6 +59,10 @@ function studio_companion_enqueue_scripts() {
             text-decoration: underline;
         }
 
+        #studio-companion-notice p {
+            margin: 0;
+        }
+
         #studio-companion-notice a:hover {
             text-decoration: none;
         }
